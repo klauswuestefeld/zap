9bd6050147fd98efc7db366b477ce996204a2067
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2014-03-02 23:57:36 -0300

    Sound progress.

--- src/zapman/Sound.java
+++ src/zapman/Sound.java
@@ -2,18 +2,42 @@ package zapman;
 
 import java.applet.Applet;
 import java.applet.AudioClip;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Sound {
 
+	private static Map<String, Deque<AudioClip>> clipPoolByName = new HashMap<>();
+	
 	public static void play(final String fileName) {
-		try {
-			AudioClip clip = Applet.newAudioClip(Sound.class
-					.getResource(fileName + ".wav"));
+		new Thread() { @Override public void run() {
+			AudioClip clip = borrowClip(fileName);
 			clip.play();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
+			try { Thread.sleep(3000); } catch (InterruptedException e) {}
+			clip.stop();
+			returnClip(fileName, clip);
+		}}.start();
 	}
 
+	synchronized
+	private static AudioClip borrowClip(final String fileName) {
+		Deque<AudioClip> clipPool = clipPoolByName.get(fileName);
+		if (clipPool == null) {
+			clipPool = new ArrayDeque<>(); //Basically a stack
+			clipPoolByName.put(fileName, clipPool);
+		}
+		AudioClip clip = clipPool.pollFirst();
+		if (clip == null) {
+			clip = Applet.newAudioClip(Sound.class.getResource(fileName + ".wav"));
+			clipPool.addFirst(clip);
+		}
+		return clip;
+	}
+
+	synchronized
+	static private void returnClip(final String fileName, AudioClip clip) {
+		clipPoolByName.get(fileName).addFirst(clip);
+	}
 }
