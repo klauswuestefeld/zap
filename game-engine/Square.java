672a4acd6c8b8037659a45691b778f144988c20c
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2014-07-27 23:24:35 -0300

    Square neighbors

--- game-engine/Square.java
+++ game-engine/Square.java
@@ -1,13 +1,24 @@
+import java.util.HashMap;
+import java.util.Map;
+
 class Square {
 
 	Thing thing;
 
+	Map<Direction, Square> neighbors = new HashMap<Direction, Square>();
+
+	
 	boolean isEmpty() {	return thing == null; }
+	
 	
 	void put(Thing other) {
 		if (thing != null) throw new IllegalStateException("Thomas, you cannot put " + other + " into a square that already contains a " + thing + ".");
 		thing = other;
 	}
+	
+	
+	void clear() { thing = null; }
+	
 	
 	void accept(Thing other) {
 		if (thing == null) {
@@ -28,6 +39,12 @@ class Square {
 		if (thing != null && other != null) throw new IllegalStateException("Thomas, " + thing + " and " + other + " cannot occupy the same square.");
 		thing = other;
 	}
+
+	
+	Square neighbor(Direction direction) {
+		return neighbors.get(direction);
+	}
+	
 	
 	@Override
 	public String toString() {
