d9f67db97d19b4909bfb7f660f8f306788537632
AlanGov <govieralan@gmail.com>
2014-10-08 17:29:15 -0300

    pedestrian random movements

--- alan-game/Pedestrian.java
+++ alan-game/Pedestrian.java
@@ -1,18 +1,37 @@
+import javax.swing.plaf.basic.BasicBorders.RadioButtonBorder;
+
 
 class Pedestrian extends Thing {
 
 	
 	void act() {
 		if (square.neighbor(down)==null){
-			direction = up;
-			step();
-			step();
-			step();
-			direction = none;
+			backToTop();
 			return;
 		}
 		direction = down;
 	    step();
+		direction = none;
+	}
+
+	void backToTop() {
+		direction = up;
+		step();
+		step();
+		step();
+		direction = left;
+		step();
+		step();
+		step();
+		step();
+		step();
+		direction = right;
+
+		int steps = random(5);
+		while (steps > 0){
+			step();
+			steps = steps - 1;
+		}
 		direction = none;
 	}
 	
